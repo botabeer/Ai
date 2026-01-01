ba9db92bf0d038cf26aabf1e95676a6c51dfb7b6
عبير <alqaws151@gmail.com>
2026-01-01 03:51:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -351,7 +351,7 @@ def get_ai_response(user_id: str, message: str) -> str:
             genai.configure(api_key=key)
             
             model = genai.GenerativeModel(
-                'gemini-1.5-flash-latest',
+                'gemini-1.5-flash',
                 generation_config=genai.types.GenerationConfig(
                     temperature=0.95,
                     top_p=0.95,
